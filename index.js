b0a391d868e799df924416a04b267e3bb4f70e2a
Brian Jacobel <bjacobel@gmail.com>
2016-07-21 23:19:06 -0400

    Export NodeParser

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var AWS = require('aws-sdk');
 
-module.exports = AWS
+// Because we build with Webpack, the AWS SDK wants to use its browser_parser, which depends on window.DOMParser
+// Export the node_parser, so people can override this behavior if their env doesn't have a native DOMParser (like React Native)
+AWS.NodeParser = require('aws-sdk/lib/xml/node_parser');
+
+module.exports = AWS;
